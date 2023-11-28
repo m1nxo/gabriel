8bb72114a89a594fad0a02d2457228220ac83971
Minseo Kim <80079020+m1nxo@users.noreply.github.com>
2023-11-28 13:02:08 -0600

    Update server.py

--- examples/one_way/server.py
+++ examples/one_way/server.py
@@ -1,16 +1,20 @@
+# this code appears to set up a server using ZeroMQ and WebSockets, configuring logging settings and defining parameters for the server's operation, including the ports and processing limits
+
 import common
 from gabriel_server.network_engine import server_runner
 
-
+# string format used to create a ZeroMQ (ZMQ) address
 ZMQ_ADDRESS_FORMAT = 'tcp://*:{}'
 
-
 def main():
+    # configure logging settings
     common.configure_logging()
-    zmq_address = ZMQ_ADDRESS_FORMAT.format(common.ZMQ_PORT)
-    server_runner.run(common.WEBSOCKET_PORT, zmq_address, num_tokens=2,
-                      input_queue_maxsize=60)
 
+    # server will listen for connections
+    zmq_address = ZMQ_ADDRESS_FORMAT.format(common.ZMQ_PORT)
+
+    # the port number for WebSocket communication, ZMQ address, control the number of tokens or available processing resources for incoming requests, setting a maximum size for the input queue
+    server_runner.run(common.WEBSOCKET_PORT, zmq_address, num_tokens=2, input_queue_maxsize=60)
 
 if __name__ == '__main__':
     main()
